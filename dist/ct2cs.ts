9cdb43bfd922bc69b7dfd48c264bdb9d01e12f14
Z Yin <Z.Yin.MD@gmail.com>
2024-07-23 23:01:55 -0400

    improve js doc

--- dist/ct2cs.ts
+++ dist/ct2cs.ts
@@ -1,12 +1,15 @@
 import { map } from "../dist/map.ts";
 
 /**
- * Convert a string from traditional Chinese to simplified Chinese. Non-chinese characters will remain unchanged. Chinese characters that are the same in traditional and simplified Chinese will remain unchanged.
+ * Convert a string from traditional Chinese to simplified Chinese.
+ * @param input input string
+ * @returns output string. Non-chinese characters will remain unchanged. Chinese characters that are the same in traditional and simplified Chinese will remain unchanged.
  */
 export function ct2cs(input: string): string {
   if (typeof input !== "string") return input;
-  return input
+  const output = input
     .split("")
     .map((c) => map[c] || c)
     .join("");
+  return output;
 }
